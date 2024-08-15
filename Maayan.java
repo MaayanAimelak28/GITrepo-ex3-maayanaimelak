5d03794b3844e27b5615ab0b967196dd38ebc888
maayan <maayanaimelak@gmail.com>
2024-08-15 12:30:08 +0300

    change Maayan to goodbye

--- Maayan.java
+++ Maayan.java
@@ -1,7 +1,7 @@
 public class Maayan{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Maayan aimelak "+i);
+            System.out.println("goodbye Maayan "+i);
         }
     }
 }
